7823f22dfc020a0a5ab22ea53ff1030e2303c5b0
liu-ming-hao <liuminghaod@foxmail.com>
2020-12-21 09:55:47 +0800

    系统搭建:集成oracle + mybatis-plus

--- provider-server-demo-two/src/main/java/com/de/providerserverdemotwo/ProviderServerDemoTwoApplication.java
+++ provider-server-demo-two/src/main/java/com/de/providerserverdemotwo/ProviderServerDemoTwoApplication.java
@@ -1,11 +1,13 @@
 package com.de.providerserverdemotwo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient   //开启   服务发现
+@MapperScan("com.de.providerserverdemotwo.dao")
 public class ProviderServerDemoTwoApplication {
 
     public static void main(String[] args) {
--- provider-server-demo/src/main/java/com/de/providerserverdemo/ProviderServerDemoApplication.java
+++ provider-server-demo/src/main/java/com/de/providerserverdemo/ProviderServerDemoApplication.java
@@ -1,11 +1,13 @@
 package com.de.providerserverdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient    //开启  服务发现:提供者
+@MapperScan("com.de.providerserverdemo.dao") //扫描dao接口包
 public class ProviderServerDemoApplication {
 
     public static void main(String[] args) {

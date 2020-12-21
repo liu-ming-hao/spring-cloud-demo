d1f53d9fe6a5797a0b2050421449c9dfef1c7dc8
liu-ming-hao <liuminghaod@foxmail.com>
2020-12-21 14:46:26 +0800

    配置zuul、ribbon、hystrix超时时间，开启feign熔断机制。yml文件多环境拆分调用

--- consumer-server-demo/src/main/java/com/de/consumerserverdemo/feign/UserFeignClient.java
+++ consumer-server-demo/src/main/java/com/de/consumerserverdemo/feign/UserFeignClient.java
@@ -1,9 +1,7 @@
 package com.de.consumerserverdemo.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * UserFeignClient
@@ -12,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Description   feign跨服务调用
  * @since 2020/12/8 13:36
  */
-@FeignClient(name="provider-service-demo")
+@FeignClient(name="provider-service-demo",fallbackFactory = UserFeignClientFallback.class)
 public interface UserFeignClient {
 
-    @RequestMapping(value="/provider/hi",method = RequestMethod.GET)
+    @GetMapping(value="/provider/hi")
     String home(@RequestParam String name);
 }

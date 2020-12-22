78761d9abc3480076634cd01b6470a92274fa3f1
liu-ming-hao <liuminghaod@foxmail.com>
2020-12-22 08:18:16 +0800

    用户服务 fallback类

---
+++ consumer-server-demo/src/main/java/com/de/consumerserverdemo/feign/UserFeignClientFallback.java
@@ -0,0 +1,28 @@
+package com.de.consumerserverdemo.feign;
+
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * UserFeignClientFailBack
+ *
+ * @author 刘明浩
+ * @Description
+ * @since 2020/12/21 10:52
+ */
+@Component
+@Slf4j
+public class UserFeignClientFallback implements FallbackFactory<UserFeignClient> {
+
+    @Override
+    public UserFeignClient create(Throwable throwable) {
+        return new UserFeignClient() {
+            @Override
+            public String home(String name) {
+                log.info("User-Feign fallback: " + throwable);
+                return "User-Feign fallback:" + throwable;
+            }
+        };
+    }
+}

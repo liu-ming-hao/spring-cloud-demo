78d7ce436a8cc208b5e8fef36e68d4f95ef6ae73
liu-ming-hao <liuminghaod@foxmail.com>
2020-12-22 08:42:41 +0800

    测试类

--- provider-server-demo/src/test/java/com/de/providerserverdemo/ProviderServerDemoApplicationTests.java
+++ provider-server-demo/src/test/java/com/de/providerserverdemo/ProviderServerDemoApplicationTests.java
@@ -1,14 +1,33 @@
 package com.de.providerserverdemo;
 
 
+import com.de.providerserverdemo.dao.UserDao;
+import com.de.providerserverdemo.model.User;
+import com.de.providerserverdemo.service.IUserService;
+import javafx.application.Application;
+import lombok.SneakyThrows;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
 
 @SpringBootTest
-class ProviderServerDemoApplicationTests {
+public class ProviderServerDemoApplicationTests {
+    @Autowired
+    IUserService iUserService;
+
+    @Autowired
+    UserDao thisMapper;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        User user = new User();
+        List<User> res = iUserService.findList(user);
+        System.out.println("ok");
     }
 
 }

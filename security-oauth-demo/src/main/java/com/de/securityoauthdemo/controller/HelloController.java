02097c90292dbbcc0617aaaef3c36177cd8e6efb
liuminghao <liuminghao@qzintel.com>
2020-12-27 19:54:40 +0800

    securitydemo 手机短信验证   记住我功能

--- security-oauth-demo/src/main/java/com/de/securityoauthdemo/controller/HelloController.java
+++ security-oauth-demo/src/main/java/com/de/securityoauthdemo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.de.securityoauthdemo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020/12/25 8:25
  */
 @RestController
+@RequestMapping("/user")
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return  "hello";
     }
+
+
+    /**
+     * 获取当前登录用户信息, 方式2 注入 Authentication
+     * @return
+     */
+//    @RequestMapping("/user/info")
+//    @ResponseBody
+//    public Object userInfo(Authentication authentication) {
+//        return authentication.getPrincipal();
+//    }
+
+
+
+    /**
+     * 获取当前登录用户信息, 方式3 注入 UesrDetails
+     * @return
+     */
+    @GetMapping("/info")
+    public Object userInfo2(@AuthenticationPrincipal UserDetails userDetails) {
+        return userDetails; }
 }
